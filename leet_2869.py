ce3beb48899efc3c448234c504edddcdad4fa4d3
hawkeye <hawkeye@hawk.hawk>
2024-02-23 11:10:31 -0600

    problem solved pvs

---
+++ leet_2869.py
@@ -0,0 +1,22 @@
+class Solution:
+    def __init__(self, nums: list[int], k : int):
+        self.nums = nums 
+        self.k = k 
+    def Operation(self):
+        seen = set() 
+        for i, num in enumerate(reversed(self.nums)):
+            if num > self.k:
+                continue 
+            seen.add(num) 
+            if len(seen) == k:
+                print(seen)
+                return i + 1 
+
+# nums = [3,1,5,4,2]
+# k = 5
+            
+nums = [3,2,5,3,1]
+k = 3
+
+x = Solution(nums, k)
+print(x.Operation())
